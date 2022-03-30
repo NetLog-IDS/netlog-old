8a243eb681181d2d5feda1e49b25c13f4027da8b
Adrian Costin <adriancostin6@gmail.com>
2022-03-30 14:48:16 +0200

    Replace "option-parser" with external project
    * Moved the option parser into it's own repository as a separate project
      and added it with CMake Fetch Content.
    * Replaced old calls with newly added project library calls
    * Rewrote the code to use newly created library

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,54 @@
 #include <stdexcept>
 #include <iostream>
 
+#include <cclap/cclap.h>
 #include "capture.h"
-#include "utils/option-parser.h"
-
 
 int main(int argc, char *argv[]) {
-//    try {
-        nped::CmdlineOptionParser opt_parser(argc, argv);
+    try {
+        cclap::ArgParser ap(argc, argv);
 
-        auto args = opt_parser.args();
-        auto flags = opt_parser.flags();
-        auto switches = opt_parser.switches();
+        // Get args
+        auto args = ap.args();
+        auto flags = ap.flags();
+        auto switches = ap.switches();
 
-//        if (iface.size() != 1 || filter.empty()) {
-//            throw std::invalid_argument("Invalid input arguments");
-//        }
-//
-//        std::string pcap_filter = "";
-//        for (auto s: filter) {
-//            pcap_filter.append(s);
-//            pcap_filter.append(" ");
-//        }
-//
-//        nped::PacketSniffer ps(nped::SnifferType::FileSniffer,
-//                               iface[0].data(),
-//                               pcap_filter.data());
-//
-//        ps.run_sniffer();
-//    } catch (std::invalid_argument& e) {
-//        std::cerr << e.what() << std::endl;
-//        std::cerr << "Usage: ./nped -i INTERFACE -f filter" << std::endl;
-//        return -1;
-//    } catch (std::exception& e) {
-//        std::cerr << e.what() << std::endl;
-//        return -1;
-//    }
+        // Search for interface and filters
+        auto iface = 
+            std::find_if(flags.begin(),
+                         flags.end(),
+                         [](auto pair) { return pair.first == "i"; });
+        auto filter = 
+            std::find_if(flags.begin(),
+                         flags.end(),
+                         [](auto pair) { return pair.first == "f"; });
+
+        // Interface is mandatory for now, TODO capture from default interface if none is specified
+        if (iface == flags.end()) {
+            throw std::invalid_argument("Invalid input arguments");
+        }
+
+        // Populate filters if present
+        std::string pcap_filter = "";
+        if (filter != flags.end()) {
+            for (auto s: filter->second) {
+                pcap_filter.append(s);
+                pcap_filter.append(" ");
+            }
+        }
+
+        nped::PacketSniffer ps(nped::SnifferType::FileSniffer,
+                               iface->second[0].data(),
+                               pcap_filter.data());
+
+        ps.run_sniffer();
+    } catch (std::invalid_argument& e) {
+        std::cerr << e.what() << std::endl;
+        std::cerr << "Usage: ./nped -i INTERFACE -f filter" << std::endl;
+        return -1;
+    } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return -1;
+    }
     return 0;
 }
