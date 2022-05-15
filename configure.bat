7a53cd957d0b41aab8274c5ae2e5806a8d2b29bd
Adrian Costin <adriancostin6@gmail.com>
2022-05-15 22:19:02 +0200

    Add configuration scripts

---
+++ configure.bat
@@ -0,0 +1,1 @@
+cmd /C setup\setup.bat
---
+++ configure.sh
@@ -0,0 +1,13 @@
+root=$(which sudo)
+root=$(which doas)
+
+platform="unknown"
+unameis=$(uname)
+
+if [ $unameis = "Darwin" ]; then
+    platform="mac"
+elif [ $unameis = "Linux" ]; then
+    platform="linux"
+fi
+
+"$root" ./setup/setup.sh $platform
