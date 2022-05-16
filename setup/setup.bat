29db7f14658d97740a54868bb8b17cfdc2102b4e
Adrian Costin <adriancostin6@gmail.com>
2022-05-16 03:57:28 +0200

    fix command order in setup script

--- setup/setup.bat
+++ setup/setup.bat
@@ -2,7 +2,7 @@
 echo "Trying to install WinPcap..."
 choco install winpcap -y
 echo "Trying to install curl..."
-choco install curl -y
+choco install wget -y
 echo "Trying to install cmake..."
 choco install cmake --installargs 'ADD_CMAKE_TO_PATH=System' -y
 echo "Trying to install make..."
@@ -11,8 +11,8 @@ choco install make -y
 :: get winpcap developer pack
 echo "Fetching WinPcap developer pack..."
 mkdir ext
-curl https://www.winpcap.org/install/bin/WpdPack_4_1_2.zip --output WpdPack_4_1_2.zip
 cd ext
+wget https://www.winpcap.org/install/bin/WpdPack_4_1_2.zip
 tar -xf WpdPack_4_1_2.zip
 del WpdPack_4_1_2.zip
 echo "Developer pack is now in spoofy\ext\'
