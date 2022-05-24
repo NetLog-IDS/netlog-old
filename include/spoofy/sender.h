7876d7dd8fdf86930e74a55f0b098c252c6df8e6
Adrian Costin <adriancostin6@gmail.com>
2022-05-24 16:14:07 +0200

    Add NetworkSender implementation

--- include/spoofy/sender.h
+++ include/spoofy/sender.h
@@ -1,14 +1,63 @@
 #ifndef _SENDER_H_
 #define _SENDER_H_
-#include "isender.h"
 
-// Follows the list of delays incrementally, starting from the smallest delay first
-class KafkaSender : public ISender {
-    virtual void send() const;
+#include <memory>
+#include <tins/tins.h>
+
+class SendingStrategy {
+public:
+    virtual ~SendingStrategy() {}  // implement this in cpp file or all hell breaks loose
+    virtual void send(Tins::PDU &p) = 0;
 };
 
-// Follows the list of delays decrementally, starting from the highest delay first
-class NetworkSender : public ISender {
-    virtual void send() const;
+/** 
+ * @brief Context for sending and receiving packets with different strategies
+ */
+class Sender {
+public:
+    Sender(std::unique_ptr<SendingStrategy> sender = nullptr);
+    ~Sender();
+
+    void send_packet(Tins::Packet &p);
+
+    void set_sender(std::unique_ptr<SendingStrategy> send_strategy);
+private:
+    std::unique_ptr<SendingStrategy> sender_;
 };
+
+ /** 
+  * @brief Sends packet to Apache Kafka using librdkafka
+  * @code
+  *     auto pkt = Tins::EthernetII(eth.src_addr(), eth.dst_addr()) /
+  *         Tins::IP(ip.src_addr(), ip.dst_addr()) /
+  *         Tins::UDP(udp.sport(), udp.dport());
+  *     SendingContext sc(std::make_unique<KafkaSender>(params);
+  *     sc.send(pkt)
+  * @endcode
+ */
+class KafkaSender : public SendingStrategy {
+    virtual void send(Tins::PDU &p);
+};
+
+ /** 
+  * @brief Sends packets using libtins, on specified interface
+  * @code
+  *     auto pkt = Tins::EthernetII(eth.src_addr(), eth.dst_addr()) /
+  *         Tins::IP(ip.src_addr(), ip.dst_addr()) /
+  *         Tins::UDP(udp.sport(), udp.dport());
+  *     SendingContext sc(std::make_unique<NetworkSender>("eth0");
+  *     sc.send(pkt)
+  * @endcode
+ */
+class NetworkSender : public SendingStrategy {
+public:
+    NetworkSender(const char *interface);
+    void set_interface(const char *interface);
+private:
+    virtual void send(Tins::PDU &pdu);
+
+    Tins::NetworkInterface interface_;
+    Tins::PacketSender packet_sender_;
+};
+
 #endif // _SENDER_H_
--- src/sender.cpp
+++ src/sender.cpp
@@ -1,13 +1,25 @@
 #include "spoofy/sender.h"
 
+#include <memory>
 #include <tins/tins.h>
 
-void KafkaSender::send() const {
+// Context
+Sender::Sender(std::unique_ptr<SendingStrategy> sender) : sender_(std::move(sender)) {}
+Sender::~Sender() = default;
+void Sender::send_packet(Tins::Packet &p) { sender_->send(*p.pdu()); } // might not work as expected
+void Sender::set_sender(std::unique_ptr<SendingStrategy> sending_strategy) { sender_ = std::move(sending_strategy); }
 
+// Sending packets to Apache Kafka
+void KafkaSender::send(Tins::PDU &p) {
+    //TODO next ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 }
 
-void NetworkSender::send() const {
-
+// Sending packets over the network
+NetworkSender::NetworkSender(const char *interface) : 
+    interface_(interface), packet_sender_(std::move(Tins::PacketSender(interface_))) {}
+void NetworkSender::set_interface(const char* interface) {
+    packet_sender_.default_interface(Tins::NetworkInterface(interface));
 }
-
-
+void NetworkSender::send(Tins::PDU &pdu) {
+    packet_sender_.send(pdu);
+}
